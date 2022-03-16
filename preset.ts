1e6d18a1eca804ded8979dd30ee6ae40e330ada4
Eric Villard <dev@eviweb.fr>
2022-03-16 11:25:39 +0100

    feat: add new laravel site scaffolding generator
    Signed-off-by: Eric Villard <dev@eviweb.fr>

--- preset.ts
+++ preset.ts
@@ -1,9 +1,49 @@
+import path from "path"
+
 export default definePreset({
 	name: 'laravel-default',
 	options: {
-		// ...
+		newLaravel: null// ...
 	},
-	handler: async() => {
+	handler: async (context) => {
+		if (context.options.newLaravel) {
+			let targetdir = path.resolve(context.options.newLaravel)
+
+			await executeCommand({
+				command: 'composer', arguments: [
+					'create-project',
+					'laravel/laravel',
+					context.options.newLaravel,
+				]
+			})
+			await executeCommand({
+				command: 'git', arguments: [
+					'init',
+					context.options.newLaravel,
+				]
+			})
+			await executeCommand({
+				command: 'git', arguments: [
+					'--git-dir=' + targetdir + '/.git',
+					'--work-tree=' + targetdir,
+					'add',
+					'.',
+				]
+			})
+			await executeCommand({
+				command: 'git', arguments: [
+					'--git-dir=' + targetdir + '/.git',
+					'--work-tree=' + targetdir,
+					'commit',
+					'-v',
+					'-sm',
+					'init: first commit',
+				]
+			})
+
+			context.applyOptions.targetDirectory = targetdir
+		}
+
 		await extractTemplates()
 		// ...
 	},
